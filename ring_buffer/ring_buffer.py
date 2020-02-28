d0d3df7e1ef4f8f2294eeb17924fe24f10d28ad8
Dakotah Huey <hueydakotah@gmail.com>
2020-02-28 18:36:00 -0500

    feat: ring buffer passes tests

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,6 +1,7 @@
 from doubly_linked_list import DoublyLinkedList
 
-
+# the oldest element in the ring buffer is overwritten with the newest element.
+# works like a queue system: FIFO
 class RingBuffer:
     def __init__(self, capacity):
         self.capacity = capacity
@@ -8,15 +9,52 @@ class RingBuffer:
         self.storage = DoublyLinkedList()
 
     def append(self, item):
-        pass
+        # create head if empty
+        if self.storage.length == 0:
+            self.storage.add_to_head(item)
+            self.current = self.storage.head
+        # insert new at tail if not at capacity
+        elif self.storage.length < self.capacity:
+            self.storage.add_to_tail(item)
+            # link to tail, creating "ring"
+            self.storage.tail.next = self.storage.head
+        else:
+            # overwrite current value, starting with head
+            self.current.value = item
+            # move forward to next oldest values
+            self.current = self.current.next
+
+        # if self.storage.length < self.capacity:
+        #     self.storage.add_to_tail(item)
+        #     self.current = self.storage.head
+        #     self.storage.tail.next = self.storage.head
+        # # overwrite
+        # elif self.current is self.storage.head:
+        #     self.storage.remove_from_tail()
+        #     self.storage.add_to_tail(item)
+        #     self.current = self.current.next
+        # elif self.current is self.storage.head:
+        #     self.storage.remove_from_head()
+        #     self.storage.add_to_head(item)
+        #     self.current = self.storage.head
+        # else:
+        #     self.current.insert_before(item)
+        #     self.current.delete()
+        #     self.current = self.current.next
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
-
-        # TODO: Your code here
-
+        # should not return any None values, even if present in ring buffer
+        current = self.storage.head
+        while True:
+            list_buffer_contents.append(current.value)
+            if current is self.storage.tail:
+                break
+            else:
+                current = current.next
         return list_buffer_contents
+
 
 # ----------------Stretch Goal-------------------
 
@@ -30,3 +68,4 @@ class ArrayRingBuffer:
 
     def get(self):
         pass
+
